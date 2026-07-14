4613e3eacb56d97db18e65865fcc4d71438f8c4c
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2026-07-14 03:02:00 +0300

    feat(users api): create users api crud routes & handlers

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(express.json());
 
 // File Reading
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/users.json`))
 
 // Route handlers
 const getAllTours = (req, res) => {
@@ -122,6 +123,118 @@ const deleteTour = (req, res) => {
   });
 };
 
+// Users route handlers
+const getAllUsers = (req, res) => {
+  res.status(200).json({
+    status: "Success",
+    results: Array.isArray(users) ? users.length : 0,
+    data: {
+      users
+    }
+  })
+}
+
+const createUser = (req, res) => {
+  if (!req.body) {
+    res.status(400).json({
+      status: "Bad Request",
+      message: "malformed/missing request data",
+    });
+    return;
+  }
+
+  const id = crypto.randomUUID()
+  const newUser = {...req.body, _id: id}
+
+  users.push(newUser)
+  fs.writeFile(`${__dirname}/dev-data/data/users.json`, JSON.stringify(users), err => {
+    console.log('users file updated successfully')
+  })
+
+  res.status(201).json({
+    status: "Success",
+    data: {
+      user: newUser
+    }
+  })
+}
+
+const getSingleUser = (req, res) => {
+  const id = req.params.id
+  const user = users.find(u => u._id === id)
+
+  if(!user) {
+    res.status(404).json({
+      status: "Not Found",
+      message: "User not found!",
+    })
+    return
+  }
+
+  res.status(200).json({
+    status: "Success", 
+    data: {
+      user
+    }
+  })
+}
+
+const updateUser = (req, res) => {
+  const id = req.params.id
+  const user = users.find(u => u._id === id)
+
+  if(!user) {
+    res.status(404).json({
+      status: "Not Found",
+      message: "User not found!",
+    })
+    return
+  }
+
+  if (!req.body) {
+    res.status(400).json({
+      status: "Bad Request",
+      message: "malformed/missing request data",
+    });
+    return;
+  }
+
+  const newUser = {...user, ...req.body}
+  const newUsers = users.map(u => u._id === id ? newUser : u)
+
+  fs.writeFile(`${__dirname}/dev-data/data/users.json`, JSON.stringify(newUsers), err => {
+    console.log('users file updated successfully')
+  })
+
+  res.status(200).json({
+    status: "Success",
+    data: newUser,
+  });
+}
+
+const deleteUser = (req, res) => {
+  const id = req.params.id
+  const user = users.find(u => u._id === id)
+
+  if(!user) {
+    res.status(404).json({
+      status: "Not Found",
+      message: "User not found!",
+    })
+    return
+  }
+
+  const newUsers = users.filter(u => u._id !== id)
+
+  fs.writeFile(`${__dirname}/dev-data/data/users.json`, JSON.stringify(newUsers), err => {
+    console.log('user deleted successfully')
+  })
+
+  res.status(204).json({
+    status: "Success",
+  });
+}
+
 // tours
 // app.get("/api/v1/tours", getAllTours);
 // app.get("/api/v1/tours/:id", getSingleTour);
@@ -130,6 +243,8 @@ const deleteTour = (req, res) => {
 // app.delete("/api/v1/tours/:id", deleteTour)
 app.route("/api/v1/tours").get(getAllTours).post(createTour);
 app.route("/api/v1/tours/:id").get(getSingleTour).patch(updateTour).delete(deleteTour);
+app.route("/api/v1/users").get(getAllUsers).post(createUser)
+app.route("/api/v1/users/:id").get(getSingleUser).patch(updateUser).delete(deleteUser)
 
 // starting the server
 app.listen(3000, () => {
