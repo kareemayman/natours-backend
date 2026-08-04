35f7c23f194beed0924d37df89fa4c4cf1807828
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2026-08-04 12:42:09 +0300

    feat(remodel tours): remodel tours to add more fields

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,6 +5,7 @@ const tourSchema = new mongoose.Schema({
     type: String,
     unique: true,
     required: [true, "a tour must have a name!"],
+    trim: true,
   },
   rating: {
     type: Number,
@@ -14,6 +15,46 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, "a tour must have a price!"],
   },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  images: [String],
+  startDates: [Date],
+  duration: {
+    type: Number,
+    required: [true, "a tour must have a duration!"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "a tour must have maxGroupSize"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "a tour must have difficulty"],
+  },
+  summary: {
+    type: String,
+    required: [true, "a tour must have a summary"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "a tour must have imageCover"],
+  },
+  priceDiscount: Number,
 })
 
 const Tour = mongoose.model("Tour", tourSchema)
