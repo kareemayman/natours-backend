556b443a73d3f1a3fc55369c8e103b0071062464
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2026-08-31 16:47:50 +0300

    feat(handle invalid ids): handle cast errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,10 @@
+const AppError = require('../utils/appError')
+
 const sendErrDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
-    error: err,
     message: err.message,
+    error: err,
     stack: err.stack,
   })
 }
@@ -23,6 +25,10 @@ const sendErrProd = (err, res) => {
   }
 }
 
+const handleCastErrorDB = err => {
+  return new AppError(`Inavlid ${err.path}: ${err.value}`, 400)
+}
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500
   err.status = err.status || "error"
@@ -30,6 +36,10 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     sendErrDev(err, res)
   } else if (process.env.NODE_ENV === "production") {
-    sendErrProd(err, res)
+    let error = {...err}
+    if (err.name === 'CastError') {
+      error = handleCastErrorDB(err)
+    }
+    sendErrProd(error, res)
   }
 }
