ddd1c46821494cc22b719cf5974367009cd07437
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2026-08-31 16:17:39 +0300

    feat(dev errors and prod errors): customize errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,35 @@
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500
-  err.status = err.status || 'error'
-
+const sendErrDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
-    message: err.message
+    error: err,
+    message: err.message,
+    stack: err.stack,
   })
+}
+
+const sendErrProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    })
+  } else {
+    console.error(err)
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!'
+    })
+  }
+}
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500
+  err.status = err.status || "error"
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrDev(err, res)
+  } else if (process.env.NODE_ENV === "production") {
+    sendErrProd(err, res)
+  }
 }
