b84b6556962b360253c74616b4ee70b4eed20f62
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2026-09-09 13:36:31 +0300

    feat(authorization)

--- controllers/authController.js
+++ controllers/authController.js
@@ -79,3 +79,12 @@ exports.protect = async (req, res, next) => {
   req.user = currentUser
   next()
 }
+
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(new AppError("You are not allowed to perform this action.", 403))
+    }
+    next()
+  }
+}
