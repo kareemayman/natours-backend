45d1abbe70a15718e08c994cb769789b532e1296
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2026-09-18 15:49:36 +0300

    feat(reviews model): write reviews schema and data model

---
+++ models/reviewModel.js
@@ -0,0 +1,51 @@
+// review, rating, createdAt, ref to tour, ref to user
+const mongoose = require("mongoose")
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, "You must provide review message"],
+    },
+    rating: {
+      type: Number,
+      min: [1, "rating must be higher than or equal to 1.0"],
+      max: [5, "rating must be less than or equal to 5.0"],
+      required: [true, "You must provide rating"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      select: false, // use internally
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Tour",
+      required: [true, "Review must belong to a tour"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must belong to a user"],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+)
+
+reviewSchema.pre(/^find/, function () {
+  this.populate({
+    path: "tour",
+    select: "-__v",
+  })
+  this.populate({
+    path: "user",
+    select: "-__v",
+  })
+})
+
+const Review = mongoose.model("Review", reviewSchema)
+
+module.exports = Review
