3cb7bd9abebbb7c93fc4764fd802bfc41ccfa02e
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2026-09-22 21:34:22 +0300

    refactor(reviews nested routes): write reviews routes as simple nested tour routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const tourController = require("../controllers/tourController")
 const authController = require("../controllers/authController")
+const reviewController = require("../controllers/reviewController")
 
 const router = express.Router()
 
@@ -18,5 +19,11 @@ router
     authController.restrictTo("admin", "lead-guide"),
     tourController.deleteTour,
   )
+
+// Reviews nested routes
+router
+  .route("/:tourId/reviews")
+  .post(authController.protect, authController.restrictTo("user"), reviewController.createReview)
+  .get(reviewController.getReviews)
 
 module.exports = router
